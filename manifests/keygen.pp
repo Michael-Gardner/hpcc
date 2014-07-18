b6f89ac7b1964be43a707267f75bfb5f5b993628
Michael Gardner <vintage910@gmail.com>
2014-07-18 20:49:59 +0000

    added default single node xml file and modified keygen.pp

--- manifests/keygen.pp
+++ manifests/keygen.pp
@@ -6,7 +6,7 @@ $hpcc_file_path = $::hpcc::hpcc_file_path
 exec { 'create keys':
   command => 'sudo /opt/HPCCSystems/sbin/keygen.sh',
   before  => Exec['copy keys'],
-  onlyif  => "bash -c \' [[ -e ${hpcc_file_path}/authorized_keys ]] \'",
+  unless  => "bash -c \' [[ -e ${hpcc_file_path}/authorized_keys ]] \'",
   path    => '/usr/bin:/bin',
 }
 
@@ -15,4 +15,9 @@ exec { 'copy keys':
   command => "cp id_rsa ${hpcc_file_path} && cp id_rsa.pub ${hpcc_file_path} && cp authorized_keys ${hpcc_file_path} && ls ${hpcc_file_path} && chmod 0644 ${hpcc_file_path}/id_rsa && chmod 0644 ${hpcc_file_path}/id_rsa.pub && chmod 0644 ${hpcc_file_path}/authorized_keys",
   path    => '/usr/bin:/bin',
 }
+
+anchor { 'hpcc::keygen::begin': }
+anchor { 'hpcc::keygen::end': }
+
+
 }
