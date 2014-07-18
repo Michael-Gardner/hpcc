128f98ded9043d2181a8636ee4c69ee1355e6a4a
Michael Gardner <vintage910@gmail.com>
2014-07-18 20:31:01 +0000

    fixed some bugs w/ the exec and xargs

--- manifests/keygen.pp
+++ manifests/keygen.pp
@@ -1,22 +1,18 @@
 class hpcc::keygen
 {
 
-${hpcc_file_path} = $::hpcc::hpcc_file_path
+$hpcc_file_path = $::hpcc::hpcc_file_path
 
 exec { 'create keys':
   command => 'sudo /opt/HPCCSystems/sbin/keygen.sh',
   before  => Exec['copy keys'],
-  onlyif  => "/bin/bash -c \' [[ -e ${hpcc_file_path}/authorized_keys ]] \'",
+  onlyif  => "bash -c \' [[ -e ${hpcc_file_path}/authorized_keys ]] \'",
+  path    => '/usr/bin:/bin',
 }
 
 exec { 'copy keys':
   cwd     => '/home/hpcc/.ssh',
-  command => "cp id_rsa ${hpcc_file_path} \
-              && cp id_rsa.pub ${hpcc_file_path} \
-              && cp authorized_keys ${hpcc_file_path} \
-              && ls ${hpcc_file_path} | \
-              xargs -I file chmod 0644 file",
+  command => "cp id_rsa ${hpcc_file_path} && cp id_rsa.pub ${hpcc_file_path} && cp authorized_keys ${hpcc_file_path} && ls ${hpcc_file_path} && chmod 0644 ${hpcc_file_path}/id_rsa && chmod 0644 ${hpcc_file_path}/id_rsa.pub && chmod 0644 ${hpcc_file_path}/authorized_keys",
   path    => '/usr/bin:/bin',
-
-
+}
 }
